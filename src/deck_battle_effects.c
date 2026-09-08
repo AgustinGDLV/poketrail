40418fbbd8df4199eb9378e3aa95cc46daaf7e12
AgustinGDLV <agustingomezdelavega@gmail.com>
2026-09-07 22:39:33 -0700

    power up uses boosts

--- src/deck_battle_effects.c
+++ src/deck_battle_effects.c
@@ -99,16 +99,16 @@ static void ApplyStatChange(u32 attacker, u32 target, u32 stat)
     if (stat == STAT_ATK || stat == 0xFF)
     {
         if (GetDeckBattlerSide(attacker) != GetDeckBattlerSide(target))
-            gDeckMons[target].defBoost -= (gDeckMons[gBattlerAttacker].power * gDeckMovesInfo[gCurrentMove].power) / 100;
+            gDeckMons[target].defBoost -= ((gDeckMons[gBattlerAttacker].power + gDeckMons[gBattlerAttacker].powerBoost) * gDeckMovesInfo[gCurrentMove].power) / 100;
         else
-            gDeckMons[target].defBoost += (gDeckMons[gBattlerAttacker].power * gDeckMovesInfo[gCurrentMove].power) / 100;
+            gDeckMons[target].defBoost += ((gDeckMons[gBattlerAttacker].power + gDeckMons[gBattlerAttacker].powerBoost) * gDeckMovesInfo[gCurrentMove].power) / 100;
     }
     if (stat == STAT_DEF || stat == 0xFF)
     {
         if (GetDeckBattlerSide(attacker) != GetDeckBattlerSide(target))
-            gDeckMons[target].powerBoost -= (gDeckMons[gBattlerAttacker].power * gDeckMovesInfo[gCurrentMove].power) / 100;
+            gDeckMons[target].powerBoost -= ((gDeckMons[gBattlerAttacker].power + gDeckMons[gBattlerAttacker].powerBoost) * gDeckMovesInfo[gCurrentMove].power) / 100;
         else
-            gDeckMons[target].powerBoost += (gDeckMons[gBattlerAttacker].power * gDeckMovesInfo[gCurrentMove].power) / 100;
+            gDeckMons[target].powerBoost += ((gDeckMons[gBattlerAttacker].power + gDeckMons[gBattlerAttacker].powerBoost) * gDeckMovesInfo[gCurrentMove].power) / 100;
     }
 }
 
